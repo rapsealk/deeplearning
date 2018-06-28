42752689fe5640183392362bd7ff6b46d7b858e3
rapsealk <piono623@naver.com>
2018-06-28 16:19:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Deeplearning
+
+## Keras Layer
+* Convolutional Layer: 가중치가 있는 filter mask를 통해 지역적인 특징값을 추출
+![Convolutional Layer](https://github.com/rapsealk/deeplearning/blob/master/resources/convolutional_layer.png)
+* Max Pooling Layer: mask를 통해 지역의 최대값을 추출
+* Flatten Layer: 입력된 n차원 행렬을 1차원 선형 벡터로 변환
+* Dense Layer: Fully-Connected Layer를 통해 연산
---
+++ mnist.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+RANDOM_SEED = 1234
+
+import numpy as np
+np.random.seed(RANDOM_SEED)
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, pooling
+from keras.utils import np_utils
+
+from keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print(X_train.shape, X_test.shape)
+print(y_train.shape, y_test.shape)
+
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+print(X_train.shape)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+# One-hot encoding
+Y_train = np_utils.to_categorical(y_train, 10)
+Y_test = np_utils.to_categorical(y_test, 10)
+print(Y_train[:10])
+
+
+# Define Model Architecture
+model = Sequential()
+model.add(Conv2D(32, 3, 3, activation='relu', input_shape=(28, 28, 1)))
+print(model.output_shape)
+
+model.add(Conv2D(32, 3, 3, activation='relu'))
+model.add(pooling.MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+print(model.output_shape)
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+print(model.output_shape)
+
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Fit model on training data
+model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1)
+
+# Evaluate model on test data
+score = model.evaluate(X_test, Y_test, verbose=0)
+print(model.metrics_names)
+print(score)
